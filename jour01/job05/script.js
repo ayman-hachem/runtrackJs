3871fb6f151bd39de5f6abf78f6dfd74743714e6
Ayman-Hachem <ayman.hachem@laplateforme.io>
2024-03-25 16:18:46 +0100

    new file:   jour01/job05/index.html new file:   jour01/job05/script.js
    job terminer

---
+++ jour01/job05/script.js
@@ -0,0 +1,4 @@
+let AfficherJoursSemaines = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche']
+for (let index = 0; index < AfficherJoursSemaines.length; index++) {
+    console.log(AfficherJoursSemaines[index])
+}
