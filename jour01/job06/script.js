29113174a8c50938ac75a475ceb56eafaa8c4b62
Ayman-Hachem <ayman.hachem@laplateforme.io>
2024-03-25 17:17:46 +0100

    new file:   jour01/job06/index.html new file:   jour01/job06/script.js  Job terminer

---
+++ jour01/job06/script.js
@@ -0,0 +1,15 @@
+for (let index = 1; index <= 151; index++) {
+
+if (index % 3 == 0 && index %5 == 0)  
+{
+    console.log( index + " Fizzbuzz");
+}
+if (index % 5 == 0)
+{
+    console.log( index + " buzz");
+}
+if (index % 3 == 0)
+{
+    console.log(index + " Fizz");
+}}
+ 
