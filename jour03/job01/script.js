2106cb023809b841b58477866685a05233354925
Ayman-Hachem <ayman.hachem@laplateforme.io>
2024-03-29 10:22:24 +0100

    job01 terminer

--- jour03/job01/script.js
+++ jour03/job01/script.js
@@ -1,30 +1,9 @@
-// $("#citation");
+$("#button").click(function()
+{
+  $("#citation").text("“Les logiciels et les cathédrales, c'est un peu la même chose - d'abord, on les construit, ensuite, on prie.”");
+  $("#citation").show();
+});
 
-// citation.text("Les logiciels et les cathédrales, c'est un peu la même chose - d'abord, on les construit, ensuite, on prie.");
-
-// button.click(function(){
-// citation.show();
-// });
-
-// button.click(function(){
-// citation.hide();
-// });
-
-button.click(function showhide() {
-$('#citation');
-let citation = $("#citation");
-    // Vérifie si l'article existe déjà
-    if ($("#citation")) {
-      // S'il existe, le cache
-      citation.parentNode.removeChild("citation");
-      citation.hide();
-    } else {
-      // S'il n'existe pas, ça le fait apparaitre
-      citation.text("Les logiciels et les cathédrales, c'est un peu la même chose - d'abord, on les construit, ensuite, on prie.");
-      let newCitation = document.createElement('citation');
-      newCitation.textContent = articleText;
-      newCitation.id = 'citation';
-      document.body.appendChild(newCitation);
-      citation.show();
-    }
-})
+$("#button1").click(function() {
+  $("#citation").hide();
+});
